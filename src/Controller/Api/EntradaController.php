e0a853d6d032adf8569b24150d5ed40b4cfb458c
Diego <diego_tapia_millan@yahoo.es>
2022-06-09 19:35:05 +0200

    añadida consulta compleja

--- src/Controller/Api/EntradaController.php
+++ src/Controller/Api/EntradaController.php
@@ -28,14 +28,55 @@ class EntradaController extends AbstractController
                 'id' => $entrada->getId(),
                 'slug' => $entrada->getSlug(),
                 'titulo' => $entrada->getTitulo(),
-                'fecha' => $entrada -> getFecha()-> format('Y-m-d H:i:s'),
-                'usuario' => $entrada -> getUsuario() -> getEmail(),
-                'categoria' => $entrada -> getCategoria() -> getNombre(),
+                'fecha' => $entrada->getFecha()->format('Y-m-d H:i:s'),
+                'usuario' => $entrada->getUsuario()->getEmail(),
+                'categoria' => $entrada->getCategoria()->getNombre(),
             ];
         }
         return $this->json([
             'result' => 'ok',
             'espacios' => $resultado
         ]);
+    }
+    /**
+     * @Route("/api/entrada/{slug}", methods={"GET"})
+     */
+    public function detail($slug, EntradaRepository $entradaRepository)
+    {
+        //Comentarios
+        $entrada = $entradaRepository->findOneBy(['slug' => $slug]);
+        if ($entrada == null) {
+            throw $this->createNotFoundException();
+        }
+        $comentarios = $entrada->getComentarios();
+        $resultadoComentarios = [];
+        foreach ($comentarios as $comentario) {
+            $resultadoComentarios[]= [
+                'fecha' => $comentario->getFecha()->format('Y-m-d H:i:s'),
+                'usuario' => $comentario->getUsuario()-> getEmail(),
+                'texto'=>$comentario->getTexto()
+            ];
+        }
+        //etiquetas
+        $etiquetas = $entrada->getEtiquetas();
+        $resultadoEtiquetas=[];
+        foreach ($etiquetas as $etiqueta) {
+            $resultadoEtiquetas[] = $etiqueta->getNombre();
+        }
+
+
+        $resultado = [
+            'titulo' => $entrada->getTitulo(),
+            'fecha'=> $entrada->getFecha()-> format('Y-m-d H:i:s'),
+            'usuario' => $entrada->getUsuario() -> getEmail(),
+            'categoria' => $entrada -> getCategoria() -> getNombre(),
+            'espacio'=> $entrada->getCategoria()->getEspacio()->getNombre(),
+            'resumen'=> $entrada->getResumen(),
+            'texto' => $entrada->getTexto(),
+            'comentario' => $resultadoComentarios,
+            'etiqueta' => $resultadoEtiquetas
+
+        ];
+        return $this->json($resultado);
     }
 }
