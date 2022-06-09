3032e49d944ad7ed786b3ec6627075d63a07b615
Diego <diego_tapia_millan@yahoo.es>
2022-06-09 18:38:53 +0200

    Filtros terminados

--- src/Controller/Api/EntradaController.php
+++ src/Controller/Api/EntradaController.php
@@ -19,7 +19,8 @@ class EntradaController extends AbstractController
     public function index(Request $request, EntradaRepository $entradaRepository, PaginatorInterface $paginator): Response
     {
         $currentPage = $request->query->get('page', 1);
-        $query = $entradaRepository->getQueryAll();
+        $filter = $request->query->all();
+        $query = $entradaRepository->getQueryByFilter($filter);
         $entradas = $paginator->paginate($query, $currentPage, 10);
         $resultado = [];
         foreach ($entradas as $entrada) {
@@ -28,6 +29,8 @@ class EntradaController extends AbstractController
                 'slug' => $entrada->getSlug(),
                 'titulo' => $entrada->getTitulo(),
                 'fecha' => $entrada -> getFecha()-> format('Y-m-d H:i:s'),
+                'usuario' => $entrada -> getUsuario() -> getEmail(),
+                'categoria' => $entrada -> getCategoria() -> getNombre(),
             ];
         }
         return $this->json([
--- src/Repository/EntradaRepository.php
+++ src/Repository/EntradaRepository.php
@@ -39,28 +39,63 @@ class EntradaRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Entrada[] Returns an array of Entrada objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Entrada[] Returns an array of Entrada objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('e')
+    //            ->andWhere('e.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('e.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Entrada
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Entrada
+    //    {
+    //        return $this->createQueryBuilder('e')
+    //            ->andWhere('e.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+    // public function getQueryByFilter($filter)
+    // {
+    //     $qb = $this->createQueryBuilder('e')
+    //         ->where('e.usuario = :val')
+    //         ->where('e.categoria = :val');
+    //     return $qb->getQuery();
+    // }
+    public function getQueryByFilter($filter)
+    {
+        $qb = $this->createQueryBuilder('e')
+            ->where('e.estado=1');
+        if (isset($filter['categoria'])) {
+            $qb->andWhere('e.categoria = :categoria');
+            $qb->setParameter('categoria', $filter['categoria']);
+        }
+        if (isset($filter['usuario'])) {
+            $qb->join('e.usuario', 'u' );
+            $qb->andWhere('u.email = :usuario');
+            $qb->setParameter('usuario', $filter['usuario']);
+        }
+        if (isset($filter['espacio'])) {
+            $qb->join('e.categoria', 'c');
+            $qb->andWhere('c.espacio = :espacio');
+            $qb->setParameter('espacio', $filter['espacio']);
+        }
+        if (isset($filter['fechadesde'])) {
+            $qb->andWhere('e.fecha >= :fechamin');
+            $qb->setParameter('fechamin', $filter['fechadesde'] . ' 00:00:00');
+        }
+        if (isset($filter['fechahasta'])) {
+            $qb->andWhere('e.fecha <= :fechamax');
+            $qb->setParameter('fechamax', $filter['fechahasta'] . ' 23:59:59');
+        }
+        return $qb->getQuery();
+    }
 }
